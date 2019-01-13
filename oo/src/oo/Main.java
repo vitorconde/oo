160a72b80a59f3bfbf5627a585fc3b22c423127f
Vitor Conde <vitor.condexd@gmail.com>
2019-01-13 12:03:34 -0200

    Limpando tudo, retomando estudos

--- oo/src/oo/Main.java
+++ oo/src/oo/Main.java
@@ -1,58 +1,10 @@
 package oo;
 
-public class Main implements PlayerInterface{
+public class Main {
 
 	public static void main(String[] args) {
 		Player player = new Player();
 		
 		player.nascer();
 	}
-
-	
-
-		/*
-		 * Iniciando Game
-
-		 * System.out.println("Game Start");
-
-
-		 * Iniciando Jogador
-		 * Iniciando Inimigo
-
-		 * Player player = new Player();
-		 * Enemy enemy = new Enemy();
-
-		 * player.iniciarPlayer();
-		 * enemy.iniciarEnemy();
-
-		new Main().instanceMain2();
-		System.out.println("Inner Class");
-
-		new TesteAbstract() {
-		};
-
-	public void iniciarJogo() {
-
-	}
-
-	public void chamarMetodoAbstract() {
-		this.iniciarJogo();
-		this.instanceMain2();
-	}
-
-	public void instanceMain2() {
-		new Main2().print();
-	}
-
-	private class Main2{
-		public void print() {
-			System.out.println("Chamando meu método");
-		}
-	}
-
-	@Override
-	public void metodo() {
-		System.out.println("Carregando....");
-	}
-		 */
-	}
+}
